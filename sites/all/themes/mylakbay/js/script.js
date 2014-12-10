d6dd5565167026d9a7208740ffbaa097806fb479
Ruthie Borces <ruthieborces@outlook.com>
2014-12-10 17:07:30 +0800

    added fonts

--- sites/all/themes/mylakbay/js/script.js
+++ sites/all/themes/mylakbay/js/script.js
@@ -3,5 +3,7 @@
     $('.page-mylakbay .main-container.container').append('<div id="left-side"></div>');
     $('.page-mylakbay .main-container.container div').first().appendTo('.page-mylakbay #left-side');
     $(".page-mylakbay #directions").appendTo('.page-mylakbay #left-side');
+    $('#fromSearchBox input').attr('placeholder', 'STARTING PLACE');
+    $('#toSearchBox input').attr('placeholder', 'DESTINATION');
   });
 }(jQuery));
