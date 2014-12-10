f784a40413b5cd3ebe111dfc4b1bf25c162d07db
Ruthie Borces <ruthieborces@outlook.com>
2014-12-10 15:49:04 +0800

    added styles and script.js

---
+++ sites/all/themes/mylakbay/js/script.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function() {
+    $('.page-mylakbay .main-container.container').append('<div id="left-side"></div>');
+    $('.page-mylakbay .main-container.container div').first().appendTo('.page-mylakbay #left-side');
+    $(".page-mylakbay #directions").appendTo('.page-mylakbay #left-side');
+  });
+}(jQuery));
